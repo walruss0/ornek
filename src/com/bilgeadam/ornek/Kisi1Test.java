ba8832bd133d2721eec279f0f123d030da4f3827
Onur Yildirim <onuryildirim5425@gmail.com>
2021-11-14 12:44:18 +0300

    Kisi ve KisiTest eklendi.

---
+++ src/com/bilgeadam/ornek/Kisi1Test.java
@@ -0,0 +1,9 @@
+package com.bilgeadam.ornek;
+
+public class Kisi1Test {
+    public static void main(String[] args) {
+        Kisi ogr1= new Kisi(1,"Cahit","Arf",5000);
+        System.out.println(ogr1);
+
+    }
+}
